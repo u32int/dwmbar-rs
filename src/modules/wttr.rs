aad4cfc9bb6b9ad91a0ed99e643bd21daf064a7a
vsh <88970656+u32int@users.noreply.github.com>
2022-05-21 18:34:02 +0200

    fix: add proper keyword handling to the wttr module

--- src/modules/wttr.rs
+++ src/modules/wttr.rs
@@ -5,9 +5,10 @@ use crate::BarModule;
 pub struct Wttr {
     pub location: &'static str,
     pub format: &'static str,
+    pub wttr_format: &'static str,
     /*
     prefix: %
-    VALID FORMATS (https://github.com/chubin/wttr.in)
+    VALID WTTR FORMATS (https://github.com/chubin/wttr.in)
     c    Weather condition,
     C    Weather condition textual name,
     x    Weather condition, plain-text symbol,
@@ -33,15 +34,34 @@ pub struct Wttr {
     pub update_interval: u32,
 }
 
-impl BarModule for Wttr {
-    fn get_value(&self) -> String {
-        let location_format = String::from("wttr.in/") + self.location + "\\?format=" + self.format; 
+impl Wttr {
+    fn get_wttr(&self) -> String {
+	let location_format = String::from("wttr.in/") + self.location + "\\?format=" + self.wttr_format; 
         let wttr = Command::new("curl").arg(location_format)
             .output().expect("weather check failed!");
         let wttr = String::from_utf8_lossy(&wttr.stdout).to_string();
 
         wttr
     }
+}
+
+impl BarModule for Wttr {
+    fn eval_keywords(&self, keywords: Vec<&str>) -> Vec<String> {
+	let evaled_keywords: Vec<String> = keywords.into_iter()
+	    .map(|keyword| {
+		match keyword {
+		    "weather" => self.get_wttr(),
+		    _ => keyword.to_string(),
+		}
+	    }).collect();
+
+	evaled_keywords
+    }
+
+    fn get_value(&self) -> String {
+        self.parse_format(self.format.to_string())
+    }
+
     fn get_timer(&self) -> u32 {
         self.update_interval
     }
